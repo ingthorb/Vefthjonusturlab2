c566f7597385823244ab3efcb731023b9ddb46df
ingthorb@gmail.com <ingthorb@gmail.com>
2016-08-30 11:35:38 +0000

    Setup

---
+++ Assignment2/Models/Course.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication.Models
+{
+    public class Course
+    {
+        public int ID{ get; set; }
+        public String CourseID { get; set;}
+        public String Semester { get; set;}
+}
+
+}
---
+++ Assignment2/Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WebApplication.Models
+{
+    public class Student
+    {
+        public string SSN{ get; set; }
+        public string Name { get; set;}
+
+}
+
+}
